cb77c76c7f75e724173578c2971a839ce92f351e
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-12-27 11:49:44 -0800

    I

---
+++ _src/src/main/java/com/tomekl007/sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package com.tomekl007.sorting;
+
+public class BubbleSort implements Sort {
+    //O(n^2) - because outer and inner loop
+    @Override
+    public void sort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+
+    }
+}
